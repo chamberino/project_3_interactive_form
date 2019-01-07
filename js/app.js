ec669dd5eafc0ac9df246e9a3bb77a092a36476c
Michael Chambers <mchambers175@gmail.com>
2019-01-07 01:43:10 -0800

    completed form validation

--- js/app.js
+++ js/app.js
@@ -1,18 +1,51 @@
 
-/*****
+/* * * * * * * *
 Sets focus on the first text field when the page is loaded
-*****/
+* * * * * * * */
 $(document).ready(function(){
   $("#name").focus();
   $("#other-title").hide()
 });
 
+/* * * * * * * *
+Validation Object contains objects for each input field. Each input object contains
+an error message string and boolean value that is updated if field is filled out correctly.
+* * * * * * * */
+let validation = {
+  email: {
+      field: 'mail',
+      isValid: false,
+      message: 'Please enter a valid email'
+    },
+  name: {
+      field: 'name',
+      isValid: false,
+      message: 'Please enter your name'
+    },
+  creditCard: {
+      field: 'cc-num',
+      isValid: false,
+      message: 'Please enter a valid credit card number between 13-16 digits long'
+    },
+  zip: {
+      field: 'zip',
+      isValid: false,
+      message: 'Please enter a valid zip code'
+    },
+  cvv: {
+      field: 'cvv',
+      isValid: false,
+      message: 'Please enter a valid CVV'
+    },
+  registered: {
+      isValid: false,
+      message: 'Please select an activity',
+    }
+}
 
-
-/*****
+/* * * * * * * *
 Adds a text field that is revealed when the "Other" option is selected from the "Job Role" drop down menu.
-*****/
-
+* * * * * * * */
 $('#title').on('change', function() {
   if($(this).val()==='other') {
     $("#other-title").show();
@@ -21,21 +54,25 @@ $('#title').on('change', function() {
   }
 })
 
-
-
-
-/*****
-Filters the color options that match the design selected in the "Design" menu.
-*****/
-
-const jsPuns = /JS puns/i;
-const jsOnly = /(heart JS)|(I ♥ JS)/i;
+//Hides all T-Shirt color options inititally
+$("#colors-js-puns").hide();
+/* * * * * * * *
+Filters the T-Shirt color options to only show those that match the design selected in the "Design" menu.
+* * * * * * * */
 $('#design').on('change', function() {
-  $("#color option[value='tomato']").removeAttr("selected");
-  $("#color option[value='cornflowerblue']").attr('selected', '');
+  //Regular expressions that will be used to filter options that don't match
+  const jsPuns = /JS puns/i;
+  const jsOnly = /(heart JS)|(I ♥ JS)/i;
+  //T-Shirt options are revealed if design theme is selected.
+  if($('#design').val()!=='Select Theme') {
+    $("#colors-js-puns").show();
+    $("#color option[value='tomato']").removeAttr("selected");
+    $("#color option[value='cornflowerblue']").attr('selected', '');
+  //If design theme is selected, colors are shown
   $.each($('#color option'), function(index, value) {
     $(value).show();
   });
+  //Colors that don't match design selected are hidden
   if(jsPuns.test($(this).val())) {
     $.each($('#color option'), function(index, value) {
       if(jsPuns.test(value.textContent) !== true) {
@@ -51,135 +88,200 @@ $('#design').on('change', function() {
       };
     })
   }
+  //hides color options
+} else {
+  $("#colors-js-puns").hide();
+  }
 })
 
-/*
-”Register for Activities” section
-Some events are at the same day and time as others. If the user selects a workshop, don't allow selection of a workshop at the same day and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
 
-When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
-
-As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.
-*/
-
-// function activityCompatibility(string) {
-//   let regex = /string/
-//   return /${string}/.test(string);
-// }
-
-
-//1. select element
-const bookedTimeRegEx = /\d?\d?\wm-\d?\d?\wm/;
-const activities = $('.activities').on('change', function(e) {
-  if(e.target.checked) {
-    console.log($(e.target).parent());
-
-    console.log($(e.target).parent()[0].innerText);
-    let bookedTime = $(e.target).parent()[0].innerText.match(bookedTimeRegEx)
-    // console.log(bookedTime)
-    bookedTime = bookedTime[0];
-    bookedTime = new RegExp(bookedTime)
-    // const activity = $('.activities label');
-    $.each($('.activities label'), function(index, value) {
-      // console.log(e.target);
-      if(bookedTime.test(value.textContent)&&value.children[0]!==e.target){
-        // $(value).hide();
-        value.children[0].disabled = true;
-        value.style.color = '#b20000';
+/* * * * *
+runningTotal variable is initialized and will be used to store total cost of selected activities
+* * * * */
+let runningTotal = 0;
+/* * * * *
+Handler updates available options as user selects activities. A running total is also displayed
+and updated each time a selection is made.
+* * * * */
+const activities = $('#activities').on('change', function(e) {
+  //Total is removed each time a selection is made.
+  $('.total').remove();
+  //Regular expressions that will be used to extract the time and cost from each option
+  const bookedTimeRegEx = /\w+day\s\d?\d\w?m-\d?\d\w?m/;
+  const costRegEx = /\$\d+/;
+  //Cost variable holds the cost of the selected events
+  let cost = $(e.target).parent()[0].innerText.match(costRegEx)[0];
+  //The number is extracted from the string and variable is updated
+  cost = parseInt(cost.replace(/(\$)(\d+)/, '$2'), 10)
+  //bookedTime holds time of the selected event
+  let bookedTime = $(e.target).parent()[0].innerText.match(bookedTimeRegEx);
+  if (e.target.checked) {
+    runningTotal += cost;
+  }
+  if (e.target.checked !== true) {
+    runningTotal -= cost;
+  }
+    /*Condition checks if bookedTime hold a valid value. If Main Conference is select_method
+    it would show up as null since there is no time associated with that option.*/
+  if (bookedTime !== null) {
+    //Regular Expression is dynamically created
+    bookedTime = new RegExp(bookedTime[0])
+    /*Options are iterated over and styles are updated based on whether user selects or
+    deselects an option and if the selected time slot conflicts with another option*/
+    $.each($('#activities label'), function(index, value) {
+      if (e.target.checked) {
+        if(bookedTime.test(value.textContent)&&value.children[0]!==e.target){
+          value.children[0].disabled = true;
+          value.style.color = '#5b5b5b';
+        }
+      }
+      if (e.target.checked !== true) {
+        if(bookedTime.test(value.textContent)&&value.children[0]!==e.target){
+          value.children[0].disabled = false;
+          value.style.color = 'black';
+        }
       }
     });
-}
+  }
+  //Validation Object is updated based on runningTotal value
+  if(runningTotal>0){
+      validation.registered.isValid = true;
+      $('.activityError').remove();
+    } else if(runningTotal<=0) {
+      validation.registered.isValid = false;
+      $('#activities').after(`<p class="activityError"style="color:#e5001a; margin-top:0;">Please select an activity</p>`);
+  }
+  //Running total is concatenated into HTML
+  $('#activities').append(`<div class="total">Total: $${runningTotal}</div>`);
 });
-//2. Use regex to f
 
-
-/*
+/* * * * * * * *
 "Payment Info" section
-Display payment sections based on the payment option chosen in the select menu.
+* * * * * * * */
+// 'Select Method' is disabled as an available option
+$("option[value='select_method']").attr('disabled', '');
+// Credit Card option is set by default
+$("option[value='credit-card']").attr('selected', '');
+// Hides paypal and bitcoin instructions
+$('.paypal').hide();
+$('.bitcoin').hide();
 
-The "Credit Card" payment option should be selected by default. Display the #credit-card div, and hide the "PayPal" and "Bitcoin" information. Payment option in the select menu should match the payment option displayed on the page.
+// Handler hides all payment options, then shows the information relevant to the selected option
+$('#payment').on('change', function(e) {
+  $('.paypal').hide();
+  $('.bitcoin').hide();
+  $('.credit-card').hide();
+  $(`.${$(this).val()}`.toLowerCase()).show();
+  //Validation Object is updated if option other than credit-card is selected.
+  if($(this).val()!=='credit-card'){
+    validation.creditCard.isValid = true;
+    validation.zip.isValid = true;
+    validation.cvv.isValid = true;
+  } else if ($(this).val()==='credit-card'){
+    validation.creditCard.isValid = false;
+    validation.zip.isValid = false;
+    validation.cvv.isValid = false;
+  }
+});
 
-When a user selects the "PayPal" payment option, the PayPal information should display, and the credit card and “Bitcoin” information should be hidden.
+/* * * * * * * *
+Handler checks if input field is empty and updates Validation Object. A placeholder attribute
+is also added for user guidance.
+* * * * * * * */
+$('#name').on('focusin', function(e) {
+  $('#name').attr('placeholder','Required')
+});
+$('#name').on('focusout', function(e) {
+  $('#name').attr('placeholder','')
+  if($('#name').val().length>0){
+    validation.name.isValid = true;
+  }
+  if($('#name').val().length===0){
+    validation.name.isValid = false;
+  }
+});
 
-When a user selects the "Bitcoin" payment option, the Bitcoin information should display, and the credit card and “PayPal” information should be hidden.
+/* * * * * * * *
+Handler adds placeholder attribute to display properly formatted email address to user.
+* * * * * * * */
+$('#mail').on('focusin', function(e) {
+  $('#mail').attr('placeholder','Required (mail@example.com)')
+});
+$('#mail').on('focusout', function(e) {
+  $('#mail').attr('placeholder','')
+  $('span.error').hide();
+});
 
-NOTE: The user should not be able to select the "Select Payment Method" option from the payment select menu, because the user should not be able to submit the form without a chosen payment option.
-
-*/
-
-
-/*
-Form validation
-If any of the following validation errors exist, prevent the user from submitting the form:
-
-Name field can't be blank.
-
-Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example.
-*/
-$('#mail').on('keypress', function(e) {
+/* * * * * * * *
+Handler checks for valid email format and displays error message until valid string is entered
+* * * * * * * */
+$('#mail').on('keyup', function(e) {
+  if($('span.error').length === 0){
+    $('#mail').after(`<div class=errorDiv><span class="error" style=display:none>Must be a valid email address</span></div>`);
+  }
   const emailRegEx = /^[^@]+@[^@.]+\.[a-z]+$/i;
-  if(emailRegEx.test($(this).val())) {
+  if(emailRegEx.test($(this).val()) || $('#mail').val()
+ === '') {
     $('span.error').hide();
+    validation.email.isValid = true;
   };
+  if(emailRegEx.test($(this).val())!==true) {
+    $('span.error').text('Must be a valid email address')
+    $('span.error').show();
+    validation.email.isValid = false;
+  }
+  if($('#mail').val().length === 0){
+    $('span.error').text('Please enter email address')
+  }
 })
 function isValidEmail(email) {
   return /^[^@]+@[^.]+.[a-z]{3}$/i.test(email)
 }
 
-/*
-User must select at least one checkbox under the "Register for Activities" section of the form.
-
-If the selected payment option is "Credit Card," make sure the user has supplied a Credit Card number, a Zip Code, and a 3 number CVV value before the form can be submitted.
-
-Credit Card field should only accept a number between 13 and 16 digits.
-
-The Zip Code field should accept a 5-digit number.
-
-The CVV should only accept a number that is exactly 3 digits long.
-NOTE: Don't rely on the built in HTML5 validation by adding the required attribute to your DOM elements. You need to actually create your own custom validation checks and error messages.
-
-NOTE: Avoid using snippets or plugins for this project. To get the most out of the experience, you should be writing all of your own code for your own custom validation.
-
-NOTE: Make sure your validation is only validating Credit Card info if Credit Card is the selected payment method.
-*/
 
 
-/*
-Form validation messages
+$('#cc-num').on('keyup', function(e) {
+  const ccRegEx = /^\d{13,16}$/;
+  if(ccRegEx.test($(this).val())){
+    validation.creditCard.isValid = true;
+  };
+  if(ccRegEx.test($(this).val()) === false){
+    validation.creditCard.isValid = false;
+  }
+});
 
-Provide some kind of indication when there’s a validation error. The field’s borders could turn red, for example, or even better for the user would be if a red text message appeared near the field.
+$('#zip').on('keyup', function(e) {
+  const zipRegEx = /^\d{5}$/;
+  if(zipRegEx.test($(this).val())){
+    validation.zip.isValid = true;
+  } else {
+    validation.zip.isValid = false;
+  }
+});
 
-The following fields should have some obvious form of an error indication:
+$('#cvv').on('keyup', function(e) {
+  const cvvRegEx = /^\d{3}$/;
+  if(cvvRegEx.test($(this).val())){
+    validation.cvv.isValid = true;
+  } else {
+    validation.cvv.isValid = false;
+  }
 
-Name field
-Email field
-Register for Activities checkboxes (at least one must be selected)
-Credit Card number (Only if Credit Card payment method is selected)
-Zip Code (Only if Credit Card payment method is selected)
-CVV (Only if Credit Card payment method is selected)
+});
 
-Note: Error messages or indications should not be visible by default. They should only show upon submission, or after some user interaction.
-*/
-
-
-/*
-Form works without JavaScript - Progressive Enhancement
-The user should still have access to all form fields and payment information if JS isn't working for whatever reason. For example, when the JS is removed from the project:
-The “Other” text field under the "Job Role" section should be visible
-All information for Bitcoin, PayPal or Credit Card payments should be visible.
-*/
-
-
-/*
-Extra Credit
-To get an "exceeds" rating, you can expand on the project in the following ways:
-
- 3 steps
-T Shirt Section
-Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
-Conditional Error Message
-Program at least one of your error messages so that more information is provided depending on the error. For example, if the user hasn’t entered a credit card number and the field is completely blank, the error message reads “Please enter a credit card number.” If the field isn’t empty but contains only 10 numbers, the error message reads “Please enter a number that is between 13 and 16 digits long.”
-Real-time Error Messages
-Program your form so that it provides a real-time validation error message for at least one text input field. Rather than providing an error message on submit, your form should check for errors and display messages as the user begins typing inside a text field. For example, if the user enters an invalid email address, the error appears as the user begins to type, and disappears as soon as the user has entered a complete and correctly formatted email address. You must accomplish this will your own JavaScript code. Do not rely on HTML5's built-in email validation.
-NOTE: If you implement the above exceeds requirements in your form, make sure you detail in your submission notes which input will have different error messages depending on the error, and which input will have "real time" validation messages, so your reviewer won't miss them by accident.
-*/
+/* * * * *
+HANDLER CHECKS VALIDATION OBJECT AND PREVENTS SUBMISSION IF ANY FALSE VALUES ARE ENCOUNTERED
+* * * * */
+$('form').on('submit', function(e) {
+  $('.error-message').remove();
+  $.each(validation, function(key, value){
+  	if(value.isValid === false) {
+      e.preventDefault();
+      $('button').after(`<p class='error-message'>${value.message}</p>`);
+      $(`#${value.field}`).addClass('error-border')
+  	}
+    if(value.isValid === true) {
+      $(`#${value.field}`).removeClass('error-border')
+    }
+  })
+})
